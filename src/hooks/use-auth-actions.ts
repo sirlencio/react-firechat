2b2d256607292b78fc1cbb2eb9fa4979980b9c00
sirlencio <carlos.l.p.251001@gmail.com>
2025-08-30 13:38:41 +0200

    login form started

---
+++ src/hooks/use-auth-actions.ts
@@ -0,0 +1,108 @@
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile, type AuthError } from "firebase/auth";
+import { useState } from "react"
+import { useAuth } from "reactfire";
+
+interface AuthActionResult {
+    success: boolean;
+    error: AuthError | null;
+}
+
+export const useAuthActions = () => {
+
+    const [loading, setLoading] = useState(false);
+    const auth = useAuth();
+
+    const login = async (data: { email: string, password: string }): Promise<AuthActionResult> => {
+        setLoading(true);
+        try {
+            await signInWithEmailAndPassword(auth, data.email, data.password);
+            return {
+                success: true,
+                error: null,
+            }
+        } catch (error) {
+            const authError = error as AuthError;
+            return {
+                success: false,
+                error: authError,
+            }
+        } finally {
+            setLoading(false);
+        }
+
+    }
+
+    const register = async (data: { email: string, password: string, displayName: string }): Promise<AuthActionResult> => {
+        setLoading(true)
+        try {
+            const currentUser = await createUserWithEmailAndPassword(auth, data.email, data.password);
+
+            if (currentUser.user) {
+                await updateProfile(currentUser.user, {
+                    displayName: data.displayName
+                });
+            }
+            return {
+                success: true,
+                error: null,
+            }
+        } catch (error) {
+            const authError = error as AuthError;
+            return {
+                success: true,
+                error: authError,
+            }
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const loginWithGoogle = async (): Promise<AuthActionResult> => {
+        setLoading(true);
+        try {
+            const provider = new GoogleAuthProvider();
+            await signInWithPopup(auth, provider);
+
+            return {
+                success: true,
+                error: null,
+            }
+        } catch (error) {
+            const authError = error as AuthError;
+            return {
+                success: false,
+                error: authError,
+            }
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const logout = async (): Promise<AuthActionResult> => {
+        setLoading(true);
+        try {
+            await signOut(auth);
+            return {
+                success: true,
+                error: null,
+            }
+        } catch (error) {
+            console.log("Error during logout:", error)
+            const authError = error as AuthError;
+            return {
+                success: false,
+                error: authError,
+            }
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return {
+        loading,
+        login,
+        register,
+        loginWithGoogle,
+        logout,
+    }
+}
--- src/pages/auth/login.page.tsx
+++ src/pages/auth/login.page.tsx
@@ -1,7 +1,35 @@
-const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
+import { Button } from "@/components/ui/button";
+import { useAuthActions } from "../../hooks/use-auth-actions";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { toast } from "sonner";
 
-export default LoginPage
+const LoginPage = () => {
+  const { loginWithGoogle } = useAuthActions();
+
+  const handleLoginWithGoogle = async () => {
+    const result = await loginWithGoogle()
+    if (result.success){
+      console.log("Login successful")
+    }else{
+      console.error("Login failed:", result.error);
+      toast.error("Login failed");
+    }
+  }
+
+  return (
+    <Card className="bg-white">
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>Login to your account using email and password or with Google</CardDescription>
+      </CardHeader>
+      <CardContent>
+        ...
+      </CardContent>
+      <CardFooter>
+        <Button onClick={handleLoginWithGoogle} className="w-full">Login with Google</Button>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default LoginPage;
